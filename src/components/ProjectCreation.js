9ae9a910ac4363eee8fec2425a68ef2db1708167
Lia Fredericks <laf335@drexel.edu>
2022-02-01 16:29:40 -0500

    Added 'Option' buttons

--- src/components/ProjectCreation.js
+++ src/components/ProjectCreation.js
@@ -10,63 +10,15 @@ function ProjectCreation () {
             <h1 class="headers">Project Creation</h1>
             <Row>
                 <Col>
-                    <p class="options">Option 1</p>
+                    <a class="btn btn-primary options" href="/upload-information">Option 1</a>
                 </Col>
                 <Col>
-                    <p class="options">Option 2</p>
+                    <a class="btn btn-primary options" href="/upload-information">Option 2</a>
                 </Col>
             </Row>
         </div>
 
-        <div> 
-            <h1 class="headers">Upload Information</h1>
-            <br></br>
-            <Row>
-                <Col>
-                    <h3 class="headers">Video Requirements</h3>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </Col>
-                <Col>
-                    <h3 class="headers">Audio Requirements</h3>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </Col>
-            </Row>
-        </div>
-
-        <div>
-            <h3>Steps</h3>
-            <br></br>
-            <Row>
-                <Col>
-                    <ol>
-                        <li>SELECT VIDEO<input type="file" id="source_vid" name="source_vid"></input></li>
-                            
-                        <li>ENTER SCRIPT<input type="file" id="script_file" name="script_file"></input></li>
-                        OR
-                        <br></br>
-                        TYPE SCRIPT
-                        <br></br>
-                        <textarea id="script_typed" name="script_typed" rows="4" cols="50">
-                            Type desired script for target video here
-                        </textarea>
-                    </ol>
-                </Col>
-                <Col>
-                    <ol start="3">
-                        <li>SELECT AUDIO<input type="file" id="source_audio" name="source_audio"></input></li>
-                        OR
-                        <br></br>
-                        RECORD AUDIO
-                    </ol>
-                </Col>
-            </Row>
-        </div>
+       
         </>
     );
 
