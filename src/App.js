58d393867e18d7ed73fda6478aee256623aea6d1
myxlinh99 <linhmydo99@yahoo.com.vn>
2022-02-01 19:45:57 -0500

    Getting Started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import HeroSection from './components/HeroSection';
+import GettingStarted from './components/GettingStarted';
+import Plans from './components/Plans';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path = '/' exact />
         </Switch>
         <HeroSection></HeroSection>
+        <GettingStarted></GettingStarted>
+        <Plans></Plans>
       </Router>
     </>
   );
---
+++ src/components/GettingStarted.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './GettingStarted.css';
+
+const GettingStarted = () => {
+  return <><div class="row">
+  <div class="column"><h3>How It Works</h3>
+  <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+  <br></br>
+  <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p></div>
+  <div class="column"><img src='/images/lecture.png' alt=''></img></div>
+</div>
+<div><h2>
+  Choose from our 2 Plans
+  </h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p></div>
+</>;
+};
+
+export default GettingStarted;
