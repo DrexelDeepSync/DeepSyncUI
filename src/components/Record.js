9a0ba90a5d5278ea1180ea56c48bf1d9cf060df7
Lia Fredericks <laf335@drexel.edu>
2022-02-08 17:23:37 -0500

    WIP audio recording w/ Recorderjs library at  https://github.com/mattdiamond/Recorderjs

---
+++ src/components/Record.js
@@ -0,0 +1,82 @@
+var gumStream;
+//stream from getUserMedia() 
+var rec;
+//Recorder.js object 
+var input;
+//MediaStreamAudioSourceNode we'll be recording 
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext;
+//new audio context to help us record 
+
+
+function toggleRecording() {
+    image = getElementById("record_button");
+
+    if (rec.recording) {
+        image.src = "./images/recording.png";
+        rec.record()
+    } else {
+        image.src = "./images/record_button.png";
+        console.log("stopButton clicked");
+        //disable the stop button, enable the record too allow for new recordings 
+        rec.stop(); //stop microphone access 
+        gumStream.getAudioTracks()[0].stop();
+        //create the wav blob and pass it on to createDownloadLink 
+        rec.exportWAV(createDownloadLink);
+    }
+}
+
+function createDownloadLink(blob) {
+    var url = URL.createObjectURL(blob);
+    var au = document.createElement('audio');
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    //add controls to the <audio> element 
+    au.controls = true;
+    au.src = url;
+    //link the a element to the blob 
+    link.href = url;
+    link.download = new Date().toISOString() + '.wav';
+    link.innerHTML = link.download;
+    //add the new audio and a elements to the li element 
+    li.appendChild(au);
+    li.appendChild(link);
+    //add the li element to the ordered list 
+    recordingsList.appendChild(li);
+}
+
+/* Simple constraints object, for more advanced audio features see
+
+https://addpipe.com/blog/audio-constraints-getusermedia/ */
+
+var constraints = {
+    audio: true,
+    video: false
+} 
+/* Disable the record button until we get a success or fail from getUserMedia() */
+
+recordButton.disabled = true;
+
+
+/* We're using the standard promise based getUserMedia()
+
+https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia */
+
+navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ..."); 
+    /* assign to gumStream for later use */
+    gumStream = stream;
+    /* use the stream */
+    input = audioContext.createMediaStreamSource(stream);
+    /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
+    rec = new Recorder(input, {
+        numChannels: 1
+    }) 
+    //start the recording process 
+    rec.record()
+    console.log("Recording started");
+}).catch(function(err) {
+    //enable the record button if getUserMedia() fails 
+    recordButton.disabled = false;
+});
