0b12ca3b0cb6c6bfa6ad6d60cb6a91bb125ae6e6
Alia Yeszhanova <aliayeszhanova@gmail.com>
2022-03-08 18:51:06 -0500

    Fixing the Latin words

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -3,15 +3,22 @@ import './GettingStarted.css';
 
 const GettingStarted = () => {
   return <><div class="row">
-  <div class="column"><h3>How It Works</h3>
-  <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+  <div class="column"><h3 className='howitworks'> How It Works</h3>
+  <p> At DeepSync, we have two main options of audio and video generation depending on how much time you have on your schedule.
+   In the fast training plan, you simply upload an audio clip of you speaking, then type a script that will be the lecture content you want to present.
+   Next, you can upload a video clip of yourself. With the power of our fast generation cloning, you will have a file to output and show in a matter of minutes.
+  </p>
   <br></br>
-  <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p></div>
+  <p> If you have a few days to spare and would like to get a headstart on some long form lecture creating, you can select our slow generation option.
+  This option will allow you to upload a multitude of files to the site and in the background we use both video and audio cloning technology to make a replica depending on what you provide!
+  </p>
+  </div>
   <div class="column"><img src='/images/lecture.png' alt=''></img></div>
 </div>
-<div><h2>
-  Choose from our 2 Plans
-  </h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p></div>
+<div>
+  <h2>Choose from our 2 Plans</h2>
+  <p>Choose from our 2 plans depending on how much time you have in your schedule. </p>
+</div>
 </>;
 };
 
