da8bd50116bf91a65249d645fac794685f6459e5
Alia Yeszhanova <aliayeszhanova@gmail.com>
2022-03-07 19:23:03 -0500

    Updates to Plans Page

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -4,8 +4,8 @@ import './Plans.css';
 
 function Plans() {
   return <><div class="row">
-  <div class="column"><Card title='Plan 1' imageUrl='/images/img-1.jpg' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'></Card></div>
-  <div class="column"> <Card title='Plan 2' imageUrl='/images/img-2.jpg' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'></Card></div>
+  <div class="column"><Card title='Plan 1' imageUrl='/images/fastGeneration_GettingStarted.png' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'></Card></div>
+  <div class="column"> <Card title='Plan 2' imageUrl='/images/slowGeneration_GettingStarted.png' body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'></Card></div>
 </div>
 </>;
 }
