aa1cf9470e92999b47ec99ec8ab83b2fe0d14753
Alia Yeszhanova <aliayeszhanova@gmail.com>
2022-03-08 19:02:52 -0500

    Adding edits to Plans and Project Creation

--- src/components/ProjectCreation.js
+++ src/components/ProjectCreation.js
@@ -19,14 +19,14 @@ function ProjectCreation () {
                     <a class="btn btn-primary options" href="/upload-information">
                         <h3 className = "fastTitle"> Option 1: </h3>
                         <img src="./images/fastGeneration_GettingStarted.png" alt="wrapkit" class="fastPlan" />
-                        <p class="fastDescription"> Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+                        <p class="fastDescription"> Have only a limited amount of time or creating your lecture last minute? Choose our fast generation plan and test the result!</p>
                     </a>
                 </Col>
                 <Col>
                     <a class="btn btn-primary options" >
                         <h3 className = "slowTitle"> Option 2: </h3>
                         <img src="./images/slowGeneration_GettingStarted.png" alt="wrapkit" class="slowPlan" />
-                        <p class="slowDescription"> Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+                        <p class="slowDescription"> Are you looking to create your lecture content with a few days to spare? Choose our slow generation plan and get even more accurate videos! </p>
                     </a>
                 </Col>
             </Row>
