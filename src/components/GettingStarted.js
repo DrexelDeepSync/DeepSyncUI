158e2ebb53cc1c02411b8261f885c41a4068ab69
Alia Yeszhanova <aliayeszhanova@gmail.com>
2022-05-10 16:50:36 -0400

    Updated UI: Organization, Margins, and Adding Elements
    Alphabetized css class properties for organization, added new stock photos, fixed margins (div element around all rendered components)

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -9,16 +9,16 @@ const GettingStarted = () => {
   return <>
   <Row>
   <Col>
-    <h3 className='howitworks'>How It Works</h3>
+    <h2 className='howitworks'>How It Works</h2>
     <p> With DeepSync, you can take less than 40 minutes of speech and transform it into hours of lecture material. </p>
     <p> Simply upload the script for your lecture, a clip of you speaking, and a video or picture of yourself, and then walk away from the computer.
      From there, we use speech cloning technology and lip-syncing to make your lectures!
     </p>
   </Col>
-  <Col><img src='/images/lecture.png' alt=''></img></Col>
+  <Col><video autoPlay loop muted className='howitworks-vid'><source src='./images/zoomLecture_HOWITWORKS.mp4' type='video/mp4'/></video></Col>
 </Row>
 <Row>
-  <h2>Get Started Now!</h2>
+  <h2 className='getstartednow-title'>Get Started Now!</h2>
 </Row>
 <Plans></Plans>
 </>;
